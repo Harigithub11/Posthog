5b3bb892a0d13a3fb355dadc3bfeb3cb89af8de0
Vojta Bartos <vojtech.b@posthog.com>
2026-03-31 21:15:19 +0200

    fixup! chore(replays): tweak the openapi spec

--- posthog/session_recordings/test/test_session_recording_playlist.py
+++ posthog/session_recordings/test/test_session_recording_playlist.py
@@ -215,28 +215,35 @@ class TestSessionRecordingPlaylist(APIBaseTest, QueryMatchingTest):
             },
         ]
 
-    def test_rejects_playlist_without_type(self) -> None:
+    @parameterized.expand(
+        [
+            [
+                "without_type",
+                {"name": "test"},
+                {
+                    "attr": None,
+                    "code": "invalid_input",
+                    "detail": "Must provide a valid playlist type: either filters or collection",
+                    "type": "validation_error",
+                },
+            ],
+            [
+                "with_unknown_type",
+                {"name": "test", "type": "tomato"},
+                {
+                    "attr": "type",
+                    "code": "invalid_choice",
+                    "detail": '"tomato" is not a valid choice.',
+                    "type": "validation_error",
+                },
+            ],
+        ]
+    )
+    def test_rejects_invalid_playlist_type(self, _name: str, playlist_data: dict, expected_response: dict) -> None:
         self._create_playlist(
-            {"name": "test"},
+            playlist_data,
             status.HTTP_400_BAD_REQUEST,
-            expected_response_json={
-                "attr": None,
-                "code": "invalid_input",
-                "detail": "Must provide a valid playlist type: either filters or collection",
-                "type": "validation_error",
-            },
-        )
-
-    def test_rejects_playlist_with_unknown_type(self) -> None:
-        self._create_playlist(
-            {"name": "test", "type": "tomato"},
-            status.HTTP_400_BAD_REQUEST,
-            expected_response_json={
-                "attr": "type",
-                "code": "invalid_choice",
-                "detail": '"tomato" is not a valid choice.',
-                "type": "validation_error",
-            },
+            expected_response_json=expected_response,
         )
 
     @parameterized.expand(
