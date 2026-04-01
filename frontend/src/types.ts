a390e347d7399eb85ababd5428395b795acf09db
Vasco de Krijger <vasco@posthog.com>
2026-04-01 11:45:09 +0200

    fix: Inconsistent exports compared to UI

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -5067,6 +5067,7 @@ export type ExportContext = (
 ) & {
     row_limit?: number // Some exports have different row limits, e.g. logs
     columns?: string[]
+    variables_override?: Record<string, any>
 }
 
 export interface ExportedAssetType {
